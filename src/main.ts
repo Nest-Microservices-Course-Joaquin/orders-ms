57dc7c23d606f0aa7a24852a8545a7916265717e
Moilab <moilab.web@gmail.com>
2026-07-02 14:07:09 +0200

    feat(): orders microservice crud

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { envs } from './config/env.validation';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main-Orders-MS');
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.PORT);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      },
+    },
+  );
+  await app.listen();
   logger.log(`Orders microservice is running on port ${envs.PORT}`);
 }
 bootstrap();
