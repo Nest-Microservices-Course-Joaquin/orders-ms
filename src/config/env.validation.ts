07bcf233ddc411ffa768e7665c1f38590d938b40
Moilab <moilab.web@gmail.com>
2026-07-02 13:41:27 +0200

    feat(): env config

---
+++ src/config/env.validation.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  PORT: z.coerce.number().int().min(1).default(3000),
+});
+
+export type EnvVars = z.infer<typeof envSchema>;
+
+export function validate(config: Record<string, unknown>) {
+  const result = envSchema.safeParse(config);
+
+  if (!result.success) {
+    throw new Error(`Config validation error: ${result.error.message}`);
+  }
+
+  return result.data;
+}
+
+export const envs = validate(process.env);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config/env.validation';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Orders-MS');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.PORT);
+  logger.log(`Orders microservice is running on port ${envs.PORT}`);
 }
 bootstrap();
